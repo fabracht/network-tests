e342192eafb7c085a7bf2bc6754f2ace86432045
Fabricio Bracht <fabracht@gmail.com>
2023-05-04 07:56:43 -0700

    add socket trait documentation

--- common/src/socket.rs
+++ common/src/socket.rs
@@ -7,15 +7,58 @@ use crate::time::DateTime;
 
 use super::error::CommonError;
 
+/// A trait representing a socket that can send and receive data.
 pub trait Socket<'a, T: AsRawFd> {
+    /// Creates a new instance of the socket from the given raw file descriptor.
     fn from_raw_fd(fd: RawFd) -> T;
+
+    /// Sends the given message over the socket.
+    ///
+    /// # Arguments
+    ///
+    /// * `message` - The message to send, which must implement the `BeBytes` trait for big-endian byte order.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the number of bytes sent and the date/time at which the message was sent, or a `CommonError` if an error occurred.
     fn send(&self, message: impl BeBytes) -> Result<(usize, DateTime), CommonError>;
+
+    /// Sends the given message to the specified socket address.
+    ///
+    /// # Arguments
+    ///
+    /// * `address` - The address to send the message to.
+    /// * `message` - The message to send, which must implement the `BeBytes` trait for big-endian byte order.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the number of bytes sent and the date/time at which the message was sent, or a `CommonError` if an error occurred.
     fn send_to(
         &self,
         address: &SocketAddr,
         message: impl BeBytes,
     ) -> Result<(usize, DateTime), CommonError>;
+
+    /// Receives data from the socket into the given buffer.
+    ///
+    /// # Arguments
+    ///
+    /// * `buffer` - The buffer to receive the data into.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the number of bytes received and the date/time at which the message was received, or a `CommonError` if an error occurred.
     fn receive(&self, buffer: &mut [u8]) -> Result<(usize, DateTime), CommonError>;
+
+    /// Receives data from the socket into the given buffer, along with the address of the sender.
+    ///
+    /// # Arguments
+    ///
+    /// * `buffer` - The buffer to receive the data into.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the number of bytes received, the address of the sender, and the date/time at which the message was received, or a `CommonError` if an error occurred.
     fn receive_from(&self, buffer: &mut [u8])
         -> Result<(usize, SocketAddr, DateTime), CommonError>;
 }
