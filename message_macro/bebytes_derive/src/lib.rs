7b62ddbfb0e9523818a1e92778df4e3e7516a4c7
Fabricio Bracht <fabracht@gmail.com>
2023-07-04 17:52:17 -0700

    update readme

--- message_macro/bebytes_derive/src/lib.rs
+++ message_macro/bebytes_derive/src/lib.rs
@@ -471,6 +471,7 @@ pub fn derive_be_bytes(input: TokenStream) -> TokenStream {
 
                 // Generate the code for the struct
                 let struct_field_names = fields.named.iter().map(|f| &f.ident).collect::<Vec<_>>();
+                // Generate the code for the constructor
                 let constructor_arg_list = fields.named.iter().map(|f| {
                     let field_ident = &f.ident;
                     let field_type = &f.ty;
